c991fc30db96e68b1c408673e9bf7e95deca11fc
RaidasGrisk <mrraidas@gmail.com>
2021-04-17 20:49:52 +0300

    limit number of connections

--- app/database.py
+++ app/database.py
@@ -4,7 +4,12 @@ import asyncio
 
 # init db object
 DATABASE_URL = f"postgresql://{db_details['user']}:{db_details['password']}@{db_details['host']}/reddit"
-database = databases.Database(DATABASE_URL)
+
+# update min / max size to deal with many fastapi/gunicorn workers.
+# (likely not true, because with 1 CPU it will spawn 1 worker).
+# else, asyncpg.exceptions.TooManyConnectionsError: sorry, too many clients already
+# leave connection space for: pgadmin / make_web_data.py processes
+database = databases.Database(DATABASE_URL, min_size=2, max_size=5)
 
 
 # test
